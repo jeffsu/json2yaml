f4cc60fa3d3baaa6742f9ba2cd7491b57bd03e0a
Jeff Su <me@jeffsu.com>
2011-08-20 02:41:01 +0800

    getting hello world to work

--- src/json2yaml.js
+++ src/json2yaml.js
@@ -1,13 +1,90 @@
-(function () { 
-  function convert(obj, ret, indent) {
-    var ret = [];
-    var type = typeof obj; 
+(function (self) { 
+  /*
+   * TODO, lots of concatenation (slow in js)
+   */
+  var spacing = "  ";
 
+  function getType(obj) {
+    if (obj instanceof Array) {
+      return 'array';
+    } else if (typeof obj == 'string') {
+      return 'string';
+    } else if (obj == null) {
+      return 'null';
+    } else {
+      return 'hash';
+    }
+  }
+
+  function convert(obj, ret) {
+    var type = getType(obj);
+
+    switch(type) {
+      case 'array':
+        convertArray(obj, ret);
+        break;
+      case 'hash':
+        convertHash(obj, ret);
+        break;
+      case 'string':
+        convertString(obj, ret);
+        break;
+      case 'null':
+        ret.push('null');
+        break;
+    }
+  }
+
+  function convertArray(obj, ret) {
+    for (var i=0; i<obj.length; i++) {
+      var ele     = obj[i];
+      var recurse = [];
+      convert(ele, recurse);
+
+      for (var j=0; j<recurse.length; j++) {
+        ret.push((j == 0 ? "- " : spacing) + recurse[j]);
+      }
+    }
+  }
+
+  function convertHash(obj, ret) {
+    for (var k in obj) {
+      var recurse = [];
+      if (obj.hasOwnProperty(k)) {
+        var ele = obj[k];
+        convert(ele, recurse);
+        var type = getType(ele);
+        if (type == 'string' || type == 'null') {
+          ret.push(normalizeString(k) + ': ' +  recurse[0]);
+        } else {
+          ret.push(normalizeString(k) + ': ');
+          for (var i=0; i<recurse.length; i++) {
+            ret.push(spacing + recurse[i]);
+          }
+        }
+      }
+    }
+  }
+
+  function normalizeString(str) {
+    if (str.match(/^[\w]+$/)) {
+      return str;
+    } else {
+      return JSON.stringify(str);
+    }
+  }
+
+  function convertString(obj, ret) {
+    ret.push(normalizeString(obj));
   }
   
-  return function(obj) {
+  self.json2yaml = function(obj) {
+    if (typeof obj == 'string') {
+      obj = JSON.parse(obj);
+    }
+
     var ret = [];
-    convert(obj, ret, 0);
+    convert(obj, ret);
     return ret.join("\n");
   };
-})();
+})(this);
